7a3a1d0584eb618c75ee89010269cc7ae053ff00
RAFI <brafimahammad@gmail.com>
2022-10-15 20:45:08 +0530

    Uploading program

---
+++ OpenBrowser/src/com/hrms/TC_01.java
@@ -0,0 +1,28 @@
+package com.hrms;
+
+public class TC_01 {
+	
+	int a=20;
+	String name="Welcome";
+	
+	
+	public void method() {
+		System.out.println(name);
+	}
+	public void method2() {
+		
+		System.out.println(a);
+	}
+	public static void main(String[] args) {
+		TC_01 obj=new TC_01();
+		obj.method();
+		obj.method2();
+		
+		
+		
+		
+		
+
+	}
+
+}
